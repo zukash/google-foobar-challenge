f25b175fa32b1a29bc58a7d53714b44026b26d1a
zukash <48586209+zukash@users.noreply.github.com>
2023-12-20 23:27:36 +0900

    Solve dont-get-volunteered

--- dont-get-volunteered/solution.py
+++ dont-get-volunteered/solution.py
@@ -1,2 +1,24 @@
+from collections import deque
+from itertools import product
+
+
 def solution(src, dest):
-    #Your code here
+    si, sj = divmod(src, 8)
+    ti, tj = divmod(dest, 8)
+
+    dq = deque([(si, sj, 0)])
+    visited = {(si, sj)}
+    while dq:
+        i, j, dist = dq.popleft()
+        if i == ti and j == tj:
+            return dist
+        for di, dj in product([-2, -1, 1, 2], repeat=2):
+            if di**2 + dj**2 != 5:
+                continue
+            ni, nj = i + di, j + dj
+            if not (0 <= ni < 8 and 0 <= nj < 8):
+                continue
+            if (ni, nj) in visited:
+                continue
+            visited.add((ni, nj))
+            dq.append((ni, nj, dist + 1))
