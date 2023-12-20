538401a44fdef04492cbe6752817ab3730e65727
zukash <48586209+zukash@users.noreply.github.com>
2023-12-20 21:40:46 +0900

    Solve re-id

--- re-id/solution.py
+++ re-id/solution.py
@@ -1,2 +1,15 @@
+def get_primes(n):
+    is_prime = [True] * n
+    for i in range(2, n):
+        if not is_prime[i]:
+            continue
+        yield i
+        for j in range(i + i, n, i):
+            is_prime[j] = False
+
+
 def solution(i):
-    #​ ​Your​ ​code​ ​here
+    assert 0 <= i <= 10000
+    primes = "".join(map(str, get_primes(100000)))
+    assert len(primes) > 10000 + 5
+    return primes[i : i + 5]
