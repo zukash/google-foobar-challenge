65520de33a2d94ca6215cc2fa8029654360c51a0
zukash <48586209+zukash@users.noreply.github.com>
2024-01-10 22:12:51 +0900

    Solve find-the-access-codes

--- find-the-access-codes/solution.py
+++ find-the-access-codes/solution.py
@@ -1,2 +1,17 @@
-def solution(l):
-    # Your code here
+from collections import defaultdict
+
+
+def solution(L):
+    MAX = 10**6
+    D = defaultdict(set)
+    for x in range(1, MAX):
+        for y in range(x, MAX, x):
+            D[y].add(x)
+
+    ans = 0
+    for i, x in enumerate(L):
+        left = sum(y in D[x] for y in L[:i])
+        right = sum(x in D[y] for y in L[i + 1 :])
+        ans += left * right
+
+    return ans
