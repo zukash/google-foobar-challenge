3484ca22109e2566f20b0c5eccfa00f57bcfe571
zukash <48586209+zukash@users.noreply.github.com>
2024-01-10 22:44:10 +0900

    Solve fuel-injection-perfection

--- fuel-injection-perfection/solution.py
+++ fuel-injection-perfection/solution.py
@@ -1,2 +1,19 @@
+from collections import deque, defaultdict
+
+
 def solution(n):
-    # Your code here
+    Q = deque([(int(n), 0)])
+    V = defaultdict(bool)
+    while Q:
+        x, c = Q.popleft()
+        if V[x]:
+            continue
+        V[x] = True
+
+        if x == 1:
+            return c
+        if x & 1:
+            Q.append((x - 1, c + 1))
+            Q.append((x + 1, c + 1))
+        else:
+            Q.append((x // 2, c + 1))
